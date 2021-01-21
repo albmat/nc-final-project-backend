464811c3c2d1cab518d82233234b5572dbf2cf02
Alba Mateos <albmatcar@gmail.com>
2021-01-21 11:08:11 +0000

    make changes to host

--- knexfile.js
+++ knexfile.js
@@ -1,56 +1,33 @@
-// const ENV = process.env.NODE_ENV || "development";
-
-// const development = {
-//   client: "pg",
-//   connection: {
-//     database: "nc_final_project",
-//   },
-//   seeds: { directory: "./db/seeds/" },
-// };
-// const test = {
-//   client: "pg",
-//   connection: {
-//     database: "nc_final_project_test",
-//   },
-//   seeds: { directory: "./db/seeds" },
-// };
-
-// const dbConfig = {
-//   development,
-//   test,
-// };
-
-// module.exports = dbConfig[ENV];
-
 const { DB_URL } = process.env;
 const ENV = process.env.NODE_ENV || 'development';
+
 const baseConfig = {
   client: 'pg',
   migrations: {
-    directory: './migrations',
+    directory: './migrations'
   },
   seeds: {
-    directory: './db/seeds',
-  },
+    directory: './db/seeds'
+  }
 };
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_final_project',
-    },
+      database: 'nc_final_project'
+    }
   },
   test: {
     connection: {
-      database: 'nc_final_project_test',
-    },
+      database: 'nc_final_project_test'
+    }
   },
   production: {
     connection: {
       connectionString: DB_URL,
       ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  },
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
 module.exports = { ...customConfig[ENV], ...baseConfig };
